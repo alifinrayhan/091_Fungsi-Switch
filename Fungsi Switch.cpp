3a61d95e5bec23fc115dd9143ccf2346d85f1ae7
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-03-25 14:42:26 +0700

    Menu pilihan

--- Fungsi Switch.cpp
+++ Fungsi Switch.cpp
@@ -32,3 +32,34 @@ float pembagian(float a, float b)
 {
     return a / b;
 }
+
+int main()
+{
+    int pilihan;
+
+    do
+    {
+        cout << "=========================" << endl;
+        cout << "MENU" << endl;
+        cout << "1. Penjumlahan " << endl;
+        cout << "2. Pengurangan " << endl;
+        cout << "3. Perkalian " << endl;
+        cout << "4. Pembagian " << endl;
+        cout << "5. EXIT" << endl;
+        cout << "Masukan pilihan 1/2/3/4/5 = ";
+        cin >> pilihan;
+    
+    } while (pilihan != 5);
+    
+
+}
+
+int main ()
+{
+    switch (pilihan)
+    {
+        case 1:
+        input();
+        cout <<
+    }
+}
